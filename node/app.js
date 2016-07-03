60cb5f97eac96e92cc1f2e5621a508cb322741d1
Sebastian Sastre <sebastian@flowingconcept.com>
2016-07-03 18:56:45 -0300

    adds headless.js which is a squeak.js copy that will be decoupled of the DOM and make it depend on node. The localStorage uses node-localstorage now

--- node/app.js
+++ node/app.js
@@ -1,9 +1,4 @@
 var run = require('./run');
-var LocalStorage = require('node-localstorage').LocalStorage;
-var localStorage = new LocalStorage('./localStorage');
 
 run();
 
-// localStorage.setItem('123', 123);
-
-// console.log('localStorage.getItem("123")',localStorage.getItem('123'));
